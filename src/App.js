73ecab735492aa934fca8edd0f592ec4bb3a50e9
jessiLeandro <jessi.profissional@gmail.com>
2019-09-25 09:21:43 -0300

    FIX

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Olá idiota, agora vai, teste 2</div>
+    <div className="App">Olá idiota, agora vai, teste 3</div>
   );
 }
 
